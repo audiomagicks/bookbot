eef00af02f63d739acb6906ab1aa30dc1d0689e4
audiomagicks <audiomagicks@gmail.com>
2024-12-26 21:11:43 -0600

    update main.py with wordcount

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        print(f.read())
+    def read_full():
+        with open("books/frankenstein.txt") as f:
+            print(f.read())
+
+    def wordcount():
+        with open("books/frankenstein.txt") as f:
+            words = f.read().split()
+            print(len(words))
+
+    wordcount()
 main()
