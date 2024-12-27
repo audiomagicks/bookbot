440d5d7788849b6a33794ffa8c85aa753563f3c8
audiomagicks <audiomagicks@gmail.com>
2024-12-26 21:34:38 -0600

    refactor python.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 def main():
-    def read_full():
-        with open("books/frankenstein.txt") as f:
-            print(f.read())
+    book_path = "books/frankenstein.txt"
+    book_string = read_full(book_path)
+    num_words = wordcount(book_string)
+    print(f"{num_words} words.")
 
-    def wordcount():
-        with open("books/frankenstein.txt") as f:
-            words = f.read().split()
-            print(len(words))
+def read_full(text):
+        with open(text) as f:
+            return f.read()
 
-    wordcount()
+def wordcount(string):
+            return len(string.split())
+
 main()
